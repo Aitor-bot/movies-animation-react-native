546515745800cb8d0ea19cb386c1180caad9d72e
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2022-01-10 05:25:03 +0100

    backdrop animation working

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect, useRef} from 'react'
-import { Animated } from 'react-native';
+import { FlatList, Animated, View, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
+import {LinearGradient} from 'expo-linear-gradient'
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 import styled from 'styled-components/native';
 import Rating from './components/Rating'
 import Genre from './components/Genre'
@@ -12,15 +14,18 @@ import * as CONSTANTS from './constants/constants'
 
 const Container = styled.View`
   flex: 1;
+  padding-top: 50px;
+  background-color: #000; 
 `
 const PosterContainer = styled.View`
   width: ${CONSTANTS.ITEM_SIZE}px;
+  margin-top: ${CONSTANTS.TOP}px;
 `
 const Poster = styled.View`
   margin-horizontal: ${CONSTANTS.SPACING}px;
   padding: ${CONSTANTS.SPACING * 2}px;
   align-items: center;
-  background-color: #FFFFFF;
+  background-color: rgba(255, 255, 255, 0.1)
   border-radius: 10px;
 `
 const PosterImage = styled.Image`
@@ -33,13 +38,31 @@ const PosterImage = styled.Image`
 const PosterTitle = styled.Text`
   font-family: Syne-Mono;
   font-size: 18px;
+  color: #FFF;
 `
 const PosterDescription = styled.Text`
   font-family: Syne-Mono;
   font-size: 12px;
+  color: #FFF;
 `
 const DummyContainer = styled.View`
     width: ${CONSTANTS.SPACER_ITEM_SIZE}px;
+`
+const BackdropContainer = styled.View`
+  width: ${CONSTANTS.WIDTH}px;
+  position: absolute;
+  height: ${CONSTANTS.BACKDROP_HEIGHT}px;
+  overflow: hidden;
+`
+const BackdropImage = styled.Image`
+  position: absolute;
+  width: ${CONSTANTS.WIDTH}px;
+  height: ${CONSTANTS.BACKDROP_HEIGHT}px;
+  `
+const ContentContainer = styled.View `
+  position: absolute;
+  width: ${CONSTANTS.WIDTH}px;
+  height: ${CONSTANTS.BACKDROP_HEIGHT}px;
 `
 
 export default function App() {
@@ -63,8 +86,48 @@ if(!loaded || !fontLoaded) {
   return <AppLoading />
 }
 
+const Backdrop = ({movies, scrollX}) => {
+  return(
+    <ContentContainer>
+      <FlatList
+        data={movies}
+        keyExtractor={item => `${item.key}-back`}
+        removeClippedSubviews={false}
+        contentContainerStyle={{width: CONSTANTS.WIDTH, height: CONSTANTS.BACKDROP_HEIGHT}}
+        renderItem={({item, index}) => {
+          if(!item.backdropPath) {
+            return null
+          }
+        const translateX = scrollX.interpolate({
+          inputRange: [(index -1) * CONSTANTS.ITEM_SIZE, index * CONSTANTS.ITEM_SIZE],
+          outputRange: [0, CONSTANTS.WIDTH]
+        })
+        return (
+          <BackdropContainer
+            as={Animated.View}
+            style={{transform:[{translateX: translateX}]}}
+          >
+
+          <BackdropImage source={{uri:item.backdropPath}} />
+          </BackdropContainer>
+        )
+        }}
+      />
+      <LinearGradient
+      colors={['rgba(0, 0, 0, 0)', 'black']}
+      style={{
+        height: CONSTANTS.BACKDROP_HEIGHT,
+        width: CONSTANTS.WIDTH,
+        position: 'absolute',
+        bottom: 0,
+      }}
+    />
+    </ContentContainer>
+  )
+}
 return (
   <Container>
+    <Backdrop movies={movies} scrollX={scrollX}/>
     <StatusBar />
     <Animated.FlatList
     onScroll={Animated.event(
@@ -107,6 +170,7 @@ return (
       )
     }}
     />
+    
   </Container>
 );
 
