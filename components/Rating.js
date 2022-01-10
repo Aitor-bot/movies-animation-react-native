fd40340fbb2cd853c1091dcf98fd87535825e238
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2022-01-10 01:50:31 +0100

    align items bug fixed

--- components/Rating.js
+++ components/Rating.js
@@ -1,19 +1,18 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/native";
 import {MaterialCommunityIcons} from '@expo/vector-icons'
 
 const Container = styled.View`
     flex-direction: row;
     margin-vertical: 4px;
-    align-item: center;
+    align-items: center;
     justify-content: center;
-
 `
-export default function Rating ({rating}){
+export default function Rating({rating}){
     const totalOfFullStars = Math.floor(rating / 2)
     const starOutLineArray = Array(5 - totalOfFullStars).fill('star-outline')
-    const fullStarsArray = Array(totalOfFullStars).fill('star')
-    const ratingStars = [...fullStarsArray, ...starOutLineArray]
+    const fullStarArray = Array(totalOfFullStars).fill('star')
+    const ratingStars = [...fullStarArray, ...starOutLineArray]
 
     return (
         <Container>
@@ -23,8 +22,3 @@ export default function Rating ({rating}){
         </Container>
     )
 }
-
-const totalOfFullStars = Math.floor(rating / 2)
-const starOutLineArray = Array(5 - totalOfFullStars).fill('star-outline')
-const fullStarArray = Array(totalOfFullStars).fill('star')
-const ratingStars = [...fullStarArray, ...starOutLineArray]
